ac42dc4a44d786762b66e0c6f87d830e2a37b715
aarteaga53 <andrewarteaga123@gmail.com>
2022-12-05 13:51:49 -0800

    Added background image

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React, { useState } from 'react'
 
 function App() {
   let [color, setColor] = useState('#FFFFFF')
-  let [contrast, setContrast] = useState('#FFFFFF')
+  let [contrast, setContrast] = useState('#000000')
+  let [index, setIndex] = useState(0)
+  let [style, setStyle] = useState({color: contrast, backgroundColor: color})
 
   /**
    * generates six random characters from a string to
@@ -17,9 +19,12 @@ function App() {
         newColor += hex.charAt(Math.floor(Math.random() * hex.length))
     }
 
+    let invert = invertHex(newColor)
+
     console.log(newColor)
     setColor('#' + newColor)
-    setContrast('#' + invertHex(newColor))
+    setContrast('#' + invert)
+    setStyle({color: '#' + newColor, backgroundColor: '#' + invert})
   }
 
   /**
@@ -32,14 +37,23 @@ function App() {
     return (Number(`0x1${hex}`) ^ 0xFFFFFF).toString(16).substring(1)
   }
 
+  let enter = (e) => {
+    // setStyle({color: color, backgroundColor: contrast})
+    setIndex(parseInt(e.target.id))
+  }
+
+  let leave = () => {
+    setIndex(0)
+  }
+
   return (
     <div>
       <div className='top-panel'>
         <div className='name' style={{color: color}} onClick={randomColor}>Andrew Arteaga</div>
         <div className='links'>
-          <div className='link' style={{color: contrast}}>Home</div>
-          <div className='link' style={{color: contrast}}>About</div>
-          <div className='link' style={{color: contrast}}>Contact</div>
+          <div className='link' id='1' style={index === 1 ? style : null} onMouseEnter={enter} onMouseLeave={leave}>Home</div>
+          <div className='link' id='2' style={index === 2 ? style : null} onMouseEnter={enter} onMouseLeave={leave}>About</div>
+          <div className='link' id='3' style={index === 3 ? style : null} onMouseEnter={enter} onMouseLeave={leave}>Contact</div>
         </div>
       </div>
       <div className='page-body'>
