642de60e74b5bfe45d03d532967e68b0d128db91
aarteaga53 <andrewarteaga123@gmail.com>
2023-03-01 14:47:35 -0800

    mui icons

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Email, GitHub, LinkedIn, YouTube } from '@mui/icons-material'
+import EmailIcon from '@mui/icons-material/Email'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import YouTubeIcon from '@mui/icons-material/YouTube'
 
 const Footer = () => {
   return (
     <div>
       <div className='footer'>
         <div className='profile-links'>
-          <a className='profile-link' href='https://github.com/aarteaga53' target='_blank' rel='noopener noreferrer'><GitHub /></a>
-          <a className='profile-link' href='https://www.linkedin.com/in/andrew-arteaga-0b8065214/' target='_blank' rel='noopener noreferrer'><LinkedIn /></a>
-          <a className='profile-link' href='https://www.youtube.com/@andrewarteaga6321' target='_blank' rel='noopener noreferrer'><YouTube /></a>
-          <a className='profile-link' href='mailto:andrewarteaga123@gmail.com' target='_blank' rel='noopener noreferrer'><Email /></a>
+          <a className='profile-link' href='https://github.com/aarteaga53' target='_blank' rel='noopener noreferrer'><GitHubIcon /></a>
+          <a className='profile-link' href='https://www.linkedin.com/in/andrew-arteaga-0b8065214/' target='_blank' rel='noopener noreferrer'><LinkedInIcon /></a>
+          <a className='profile-link' href='https://www.youtube.com/@andrewarteaga6321' target='_blank' rel='noopener noreferrer'><YouTubeIcon /></a>
+          <a className='profile-link' href='mailto:andrewarteaga123@gmail.com' target='_blank' rel='noopener noreferrer'><EmailIcon /></a>
         </div>
         <p>&copy; {new Date().getFullYear()} Andrew Arteaga</p>
       </div>
