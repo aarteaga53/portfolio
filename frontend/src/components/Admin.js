62aadaca8f34abb5fdab81ff16f375bdcc6acb52
aarteaga53 <andrewarteaga123@gmail.com>
2023-03-09 14:06:47 -0800

    creating a chat class

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import DeleteIcon from '@mui/icons-material/Delete'
-import SendIcon from '@mui/icons-material/Send'
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import '../styles/Admin.css'
 import Title from './Title'
+import Chat from './Chat'
 
 const Admin = ({jwt, updateJWT}) => {
   let [messages, setMessages] = useState([])
-  let [chats, setChats] = useState([])
   let navigate = useNavigate()
 
   useEffect(() => {
@@ -28,30 +25,7 @@ const Admin = ({jwt, updateJWT}) => {
       }
     }   
 
-    let getChats = () => {
-      const data = JSON.parse(window.localStorage.getItem('chats'))
-      console.log(data)
-      setChats(data)
-    }
-
     getMessages()
-    getChats()
-
-    const targetNode = document.getElementById('chat-messages');
-    const config = { attributes: true, childList: true, subtree: true };
-
-    const callback = (mutationList, observer) => {
-      for (const mutation of mutationList) {
-        if (mutation.type === 'childList') {
-          targetNode.scrollTop = targetNode.scrollHeight
-        } 
-      }
-    };
-  
-    // Create an observer instance linked to the callback function
-    const observer = new MutationObserver(callback);
-    // Start observing the target node for configured mutations
-    observer.observe(targetNode, config);
   }, [jwt])
 
   /**
@@ -95,37 +69,6 @@ const Admin = ({jwt, updateJWT}) => {
     navigate('/')
   }
 
-  let chat = async (event) => {
-    event.preventDefault()
-    const form = new FormData(event.currentTarget)
-    const sentence = { sentence: form.get('sentence'), who: 'user' }
-    
-    let response = await fetch(`http://127.0.0.1:5000/chat`, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(sentence)
-    })
-    
-    let data = await response.json()
-    data.who = 'bot'
-    setChats(chats => [...chats, sentence, data])
-    
-    window.localStorage.setItem('chats', JSON.stringify(chats))
-    document.getElementById('sentence').value = ''
-  }
-
-  let popup = () => {
-    document.getElementById('popup').style.visibility = 'hidden'
-    document.getElementById('drop').style.visibility = 'visible'
-  }
-
-  let drop = () => {
-    document.getElementById('drop').style.visibility = 'hidden'
-    document.getElementById('popup').style.visibility = 'visible'
-  }
-
   return (
     <div>
       <div className='top-panel'>
@@ -145,23 +88,7 @@ const Admin = ({jwt, updateJWT}) => {
           </div>
         ))}
       </div>
-      <div className='chat-body chat-header' id='drop'>
-        <div className='chat-drop'>
-          <div className='chat-name'>Chat</div>
-          <div className='chat-up' onClick={drop}><KeyboardArrowDownIcon /></div>
-        </div>
-        <div className='chat-messages' id='chat-messages'>
-          {chats.map((chat, index) => (<div className={chat.who === 'user' ? 'chat-text chat-right' : 'chat-text chat-left'} key={index}>{chat.sentence}</div>))}
-        </div>
-        <form className='chat-form' onSubmit={chat}>
-          <input className='chat-input' id='sentence' name='sentence' type='text' placeholder='Send message...' autoComplete='off' required></input>
-          <button className='chat-icon' type='submit'><SendIcon /></button>
-        </form>
-      </div>
-      <div className='chat-popup chat-header' id='popup'>
-        <div className='chat-name'>Chat</div>
-        <div className='chat-up' onClick={popup}><KeyboardArrowUpIcon /></div>
-      </div>
+      <Chat />
     </div>
   )
 }
